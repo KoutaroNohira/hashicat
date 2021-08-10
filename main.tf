825a9da357aade543de2d70893b8b3e994488351
KoutaroNohira <88538796+KoutaroNohira@users.noreply.github.com>
2021-08-10 17:23:10 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -168,8 +168,8 @@ locals {
 }
 
 resource "aws_key_pair" "hashicat" {
-  key_name   = local.private_key_filename
-  public_key = tls_private_key.hashicat.public_key_openssh
+  key_name   = local.AKIAZVUDLESMWZEK365E
+  public_key = tls_private_key.a5kbMiOghIKTE8NJowqNx5ZK1702s+oxdXZoSnrK
 }
 
 /*
